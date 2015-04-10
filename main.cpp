2f8899c55f9793ebeba90d83a84f6c8ed5a585b8
M <masoncount@gmail.com>
2015-04-09 22:02:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,137 +1,65 @@
 #include <iostream>
+#include <cmath>
 #include <string.h>
-#include <fstream>
-#include <stdlib.h>
 #include <climits>
-#include "temp.h"
-#include "calculator.h"
 
 using namespace std;
 
 void stringConv(string &xInput);
 
 void clearBuffer();
+void clearScreen();
 
-int main()
-{
-	for(int i = 0; i < 70; i++)
-		cout << endl;
+void top(string input);
+void bottom(string input);
+void ascii(string zinput);
 
-	/* Checks for file with your name.
-	 * Creates it if it doesn't exsist
-	 */
+int main(){
 
-	ifstream inf("gfName");
-	string newName, name;
+	string iostr[][5]{
 
-	if(!inf){
-		system ("touch gfName");
-		ofstream output("gfName");
-		cout << "Hello! What is your name? ";
-		getline(cin, newName);
-		output << newName << endl;
-		ifstream fs("gfName");
-		name = newName;
+		/* Even row are inputs.
+		 * Responses are the input row number plus 1.
+	  	 */
+
+		// Greetings
+		{"hello", "hi", "howdy", "hola", "hey"},
+		{"Hi!", "Howdy", "Hey", "Hola", "Hello"},
+
+		// Greeting Questions
+		{"how are you", "hows it going", "how are you doing", "test", "test"},
+		{"I'm fine thanks", "Good youself?", "I'm doing well how about you", "Good", "Fine"}
 	};
-	
-	getline(inf, name);
 
+	string input;
 	bool stillGf = true;
+	clearScreen();
 
-	while(stillGf == true)
-	{		
+	while(stillGf = true){
 
 		cout << "[gfBot][~] ";
-		string input;
 		getline(cin, input);
 		stringConv(input);
+		clearScreen();
 
-		//Conversational I/Os
-		if(input == "hi")
-			cout << "Hello, " << name << "." << endl;
+		srand(time(NULL));
+		int rint = 0 + rand() % (5-0+1);
 
-		if(input == "how are you")
-			cout << "I'm good how are you?" << endl;
-		
-		if(input == "how are you doing")
-			cout << "I am doing well yourself?" << endl;
-		
-		if(input == "im fine thank you")
-			cout << "Thats good! What did you do today?" << endl;
-
-		if(input == "im good thank you")
-			cout << "I'm glad! What did you do today?" << endl;
-
-		if(input == "im ok")
-			cout << "Don't be short with me Mister! What have you been up to lately" << endl;
-
-		if(input == "im not feeling well")
-			cout << "Oh no! Is there anything I can do to make you feel better?" << endl;
-
-		if(input == "im feeling pretty down")
-			cout << "Is there anything I can do to make you feel better?" << endl;
-		
-		if(input == "today i went to school")
-			cout << "How did school go?" << endl;
-
-		if(input == "i mainly worked on you")
-			cout << "Did you add any cool new features I should know about?" << endl;
-		
-		//Functional I/Os
-		if(input == "clear"){
-			for(int i = 0; i < 70; i++)
-				cout << endl;
+		for(int i = 0; i < 2; i++){
+			for(int j = 0; j < 5; j++){
+				if(input.compare(iostr[i*2][j]) == 0)
+						ascii(iostr[i*2+1][rint]);
+			};
 		};
-		
-		if(input == "quit")
-			stillGf = false;
+	};
 
-		if(input == "exit")
-			stillGf = false;
-
-		if(input == "e")
-			stillGf = false;
-
-		if(input == "q")
-			stillGf = false;
-		
-		if(input == "can you convert this temp"){
-			temp();
-			clearBuffer();
-		};
-
-		if(input == "can you convert this temperature"){
-			temp();
-			clearBuffer();
-		};
-
-		if(input == "convert this temp"){
-			temp();
-			clearBuffer();
-		};
-
-		if(input == "do my math"){
-			calculator();
-			clearBuffer();
-		};
-
-		if(input == "can you help me with math"){
-			calculator();
-			clearBuffer();
-		};
-
-	};	
-	cout << "See you later!" << endl;
 	return 0;
-};
-
-
+}
 
 void stringConv(string &xInput){
-	
 	for( int i = 0; i < xInput.length(); i++){
 		xInput[i] = tolower(xInput[i]);
-	
+
 		if(ispunct(xInput[i])){
 			xInput.erase(i, 1);
 		};
@@ -141,4 +69,35 @@ void stringConv(string &xInput){
 void clearBuffer(){
 	cin.clear();
 	cin.ignore(INT_MAX, '\n');
+}
+
+void clearScreen(){
+	for(int i = 0; i < 75; i++)
+		cout << endl;
+}
+
+void top(string input){
+	for(int i = 0; i < input.length(); i++)
+		cout << "`";
+}
+
+void bottom(string input){
+	for(int i = 0; i < input.length(); i++)
+		cout << ",";
+}
+
+void ascii(string zinput){
+	cout << "       _,,,_"              << endl;
+	cout << "     .'     `'."           << endl;
+	cout << "    /     ____ \\ "        << endl;
+	cout << "   |    .'_  _\\/   |``";  top(zinput); cout << "|" << endl;
+	cout << "   /    ) a  a|    | "     << zinput << " |" << endl;
+	cout << "  /    (    > |   <,,,";    bottom(zinput); cout << "|" << endl;
+	cout << " (      ) ._  /"           << endl;
+	cout << " )    _/-.__.'`\\"         << endl;
+	cout << "(  .-'`-.   \\__ )"        << endl;
+	cout << " `/      `-./  `."         << endl;
+	cout << "  |    \\      \\  \\"     << endl;
+	cout << "  |     \\   \\  \\  \\"   << endl;
+	cout << "  |\\     `. /  /   \\ "   << endl;
 }
